db1b8cf5eb37794d8663fc38b4d1a3df4216b829
Liam B <71141670+liambambery23@users.noreply.github.com>
2020-10-24 16:27:06 -0400

    added js file with quiz questions

---
+++ questions.js
@@ -0,0 +1,27 @@
+let myQuestions = [
+    {
+        question: "When Michael organizes the Fun Run, what is he raising money for?",
+        options: ["Cancer","Rabies","Jan's baby"],
+        answer: "Rabies"
+    },
+    {
+        question: "Dwights family left him a large collection of _____",
+        options: ["Armoires", "Classic Cars", "Duvet Covers"],
+        answer: "Armoires"
+    },
+    {
+        question: "Where does Jim propose to Pam?",
+        options: ["At an elaborate going away party for Toby","A gas station","Chilis"],
+        answer: "A gas station"
+    },
+    {
+        question: "Dwight put Angela's cat ____ in the freezer",
+        options: ["Bandit","Princess","Sprinkles"],
+        answer: "Sprinkles"
+    },
+    {
+        question: "Who Started the fire?",
+        options: ["Ryan","Michael","Kelly"],
+        answer: "Ryan"
+    },
+];
